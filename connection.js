b7d781570dbecf2a72fa6f63fcd36260cf2a4043
ArnavKumar7 <78247827+ArnavKumar7@users.noreply.github.com>
2021-03-15 23:11:37 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -29,7 +29,7 @@ function studentdb(){
 	var comms=document.getElementById("comm1").value
 	axios({
 		method:'post',
-		url: "https://157.45.218.109:0/details",
+		url: "https://new-student-db.herokuapp.com/details",
 		data: {
 			student_name: name,
 			age: age,
