a239f20f8f6cbc70d1b3aa5515b2c16d5d70d874
ArnavKumar7 <78247827+ArnavKumar7@users.noreply.github.com>
2021-03-15 19:33:11 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,47 @@
+console.log("Whats up")
+
+
+/**
+axios({
+	method:'get',
+	url: "http://localhost:8000/connect",
+}).then(response => {
+	var store=response.data
+	console.log(response.data)
+	var firstdiv= document.getElementById("this is my World")
+	firstdiv.innerHTML= store.date
+})
+
+axios({
+	method:'get'
+	url: "http://localhost:8000/"
+}).then(response => {
+	var detail=response.data
+	console.log(response.data)
+	var secdiv= document.getElementById("this is my World")
+	secdiv.innerHTML=detail
+
+console.log("End")**/
+function studentdb(){
+	var name=document.getElementById("name1").value
+	var age=document.getElementById("age1").value
+	var srn=document.getElementById("SRN1").value
+	var comms=document.getElementById("comm1").value
+	axios({
+		method:'post',
+		url: "http://localhost:8000/details",
+		data: {
+			student_name: name,
+			age: age,
+			srn: srn,
+			comments: comms
+		}
+	}).then(response => {
+		var store=response.data
+		console.log(response.data)
+		var firstdiv= document.getElementById("this is my World")
+		firstdiv.innerHTML= store
+	})
+}
+
+
